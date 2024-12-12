70e85314be8dd899bc47f66690862896aa5a1573
cbysousa <cibellycastro1@gmail.com>
2024-12-11 23:28:58 -0300

    Sua mensagem de commit

--- tarefa_de_programacao_02/calculadora_oo.py
+++ tarefa_de_programacao_02/calculadora_oo.py
@@ -3,44 +3,28 @@ class Calculadora:
         self.__acumulador = 0.0
 
     def soma(self, operando_a, operando_b=None):
-        if operando_a is None and operando_b is None:
-            return self.__acumulador # Retorna o acumulador se nenhum operando for fornecido
-        elif operando_a is None:
-            self.__acumulador += operando_b
-        elif operando_b is None:
+        if operando_b is None:
             self.__acumulador += operando_a
         else:
             self.__acumulador = operando_a + operando_b
         return self.__acumulador
     
     def subtracao(self, operando_a, operando_b=None):
-        if operando_a is None and operando_b is None:
-            return self.__acumulador # Retorna o acumulador se nenhum operando for fornecido
-        elif operando_a is None:
-            self.__acumulador -= operando_b
-        elif operando_b is None:
+        if operando_b is None:
             self.__acumulador -= operando_a
         else:
             self.__acumulador = operando_a - operando_b
         return self.__acumulador
     
     def multiplicacao(self, operando_a, operando_b=None):
-        if operando_a is None and operando_b is None:
-            return self.__acumulador # Retorna o acumulador se nenhum operando for fornecido
-        elif operando_a is None:
-            self.__acumulador *= operando_b
-        elif operando_b is None:
+        if operando_b is None:
             self.__acumulador *= operando_a
         else:
             self.__acumulador = operando_a * operando_b
         return self.__acumulador
     
     def divisao(self, operando_a, operando_b=None):
-        if operando_a is None and operando_b is None:
-            return self.__acumulador
-        elif operando_a is None:
-            self.__acumulador /= operando_b
-        elif operando_b is None:
+        if operando_b is None:
             self.__acumulador /= operando_a
         else:
             self.__acumulador = operando_a / operando_b
